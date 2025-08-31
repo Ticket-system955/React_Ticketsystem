d333e3cc4e0b814cb441091aa2d8bff719fa6db8
Ticket-system955 <rhinegreenland@gmail.com>
2025-08-31 18:39:30 +0800

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -18,7 +18,7 @@ export default function Login({ setTab,setIsLoggedIn }) {
   const data = await res.json()
     if (data.status) {
       alert(data.notify || "登入成功！")
-      setIsLoggedIn(true)       
+      setIsLoggedIn(true) 
       navigate ('/')
   } else {
     alert(data.notify || "登入失敗")
