0aaf11fabfe5e29fc0d2e6472ff0f18bc8e6eef0
Ticket-system955 <ticketsystem309@gmail.com>
2025-09-06 06:14:23 +0800

    Update TotpTools.py

--- Backend/ProjectTools/TotpTools.py
+++ Backend/ProjectTools/TotpTools.py
@@ -3,21 +3,26 @@ import qrcode
 import io
 import base64
 
+#取得Totp相關物件
 class TotpTools:
-    
+
+    #系統產生密鑰
     def GetSecret(self,request):
         secret = pyotp.random_base32()
         request.session["secret"] = secret
         return secret
-    
+
+    #取得Totp物件
     def GetTotpObject(self,secret):
         totpobject = pyotp.TOTP(secret)
         return totpobject
-    
+
+    #取得Uri
     def GetUri(self,totpobject,email):
         uri = totpobject.provisioning_uri(name=email, issuer_name="GJun訂票平台")
         return uri
-    
+
+    #取得Qrcode
     def GetQRcodeSrc(self,uri):
         imgio = io.BytesIO()
         img = qrcode.make(uri)
