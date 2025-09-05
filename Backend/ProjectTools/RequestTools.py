111c2c3170e7f99a43a01084b4b5c6e67779ba7c
Ticket-system955 <ticketsystem309@gmail.com>
2025-09-06 03:57:03 +0800

    Update RequestTools.py

--- Backend/ProjectTools/RequestTools.py
+++ Backend/ProjectTools/RequestTools.py
@@ -1,8 +1,17 @@
-'''將使用者傳入的資料作例外處理跟解析'''
+'''
+1.使用時機:前端將資料傳進後端時
+2.功能:資料解析(.json())
+3.說明:若取得成功
+       則回傳->{"status":True,"notify":"前端資料取得成功 !",
+                "data":data}
+        例外處理
+          回傳->{"status":False,
+                 "notify":"GetJsonError ! message : [{type(e)} | {e}]}
+'''
 class RequestTools:
     async def GetJson(self,request):
         try:
             data = await request.json()
             return {"status":True,"notify":"前端資料取得成功 !","data":data}
         except Exception as e:
-            return {"status":False,"notify":"前端資料取得失敗 ! 失敗原因 : "+str(e)}
+            return {"status":False,"notify":"GetJsonError ! message : [{type(e)} | {e}]}
